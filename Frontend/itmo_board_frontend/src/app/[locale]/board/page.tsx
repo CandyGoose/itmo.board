980ecce3a214724e2886051d17b1a2fa3b88e6cc
Gleb Malikov Villegas ^-^ <44012947+glebmavi@users.noreply.github.com>
2024-10-15 15:57:06 +0300

    Created empty board page

---
+++ Frontend/itmo_board_frontend/src/app/[locale]/board/page.tsx
@@ -0,0 +1,10 @@
+import {useTranslations} from "next-intl";
+
+export default function Board() {
+    const t = useTranslations('Board');
+    return (
+        <div>
+            <h1>{t('test')}</h1>
+        </div>
+    );
+}
