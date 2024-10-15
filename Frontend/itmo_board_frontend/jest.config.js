3e307dc2ec616de8419ff8e74a878920c62a2133
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-15 20:02:45 +0300

    ci settings

--- Frontend/itmo_board_frontend/jest.config.js
+++ Frontend/itmo_board_frontend/jest.config.js
@@ -1,9 +1,22 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'jsdom',
+    testEnvironment: 'jest-environment-jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+    testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+    testPathIgnorePatterns: ['/node_modules/', '/.next/', '/dist/'],
     moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-    }
-};
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    },
+    collectCoverage: true,
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80
+      }
+    },
+    coverageDirectory: 'coverage',
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  };
+  
