515e975146136da0aff8bd884608bb26745c99e9
Gleb Malikov Villegas ^-^ <44012947+glebmavi@users.noreply.github.com>
2024-10-16 16:29:09 +0300

    Coverage correctly configured

--- Frontend/itmo_board_frontend/jest.config.js
+++ Frontend/itmo_board_frontend/jest.config.js
@@ -1,22 +1,32 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
+    testEnvironment: 'node',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
     testPathIgnorePatterns: ['/node_modules/', '/.next/', '/dist/'],
     moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '^@/(.*)$': '<rootDir>/src/$1',
     },
     collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
     coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80
-      }
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
+    transform: {
+        '^.+\\.(ts|tsx)$': [
+            'ts-jest',
+            {
+                tsconfig: './tsconfig.json',
+                babelConfig: true,
+            },
+        ],
     },
     coverageDirectory: 'coverage',
     coverageReporters: ['json', 'lcov', 'text', 'clover'],
-  };
-  
+};
--- Frontend/itmo_board_frontend/src/app/[locale]/components/buttons/BoardToolButton.tsx
+++ Frontend/itmo_board_frontend/src/app/[locale]/components/buttons/BoardToolButton.tsx
@@ -1,14 +1,13 @@
-import {useTranslations} from "next-intl";
-import Image from "next/image";
-import {BoardTool} from "@/globalTypes";
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
+import { BoardTool } from '@/globalTypes';
 
-
-export const BoardToolButton = ({name, icon_path, handler}: BoardTool) => {
+export const BoardToolButton = ({ name, icon_path, handler }: BoardTool) => {
     const t = useTranslations('BoardTool');
 
     return (
         <div>
-            <Image src={icon_path} alt={t(name)} onClick={handler}/>
+            <Image src={icon_path} alt={t(name)} onClick={handler} />
         </div>
     );
 };
