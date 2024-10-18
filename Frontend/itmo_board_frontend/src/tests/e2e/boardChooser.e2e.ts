64f595dfd81ff13c9eee547fd89cbac22afa1df0
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-18 12:02:24 +0300

    add e2e

---
+++ Frontend/itmo_board_frontend/src/tests/e2e/boardChooser.e2e.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Проверка отображения текста "тест"', async ({ page }) => {
+    await page.goto('http://localhost:3000/ru');
+
+    const heading = page.locator('h1');
+    await expect(heading).toHaveText('тест');
+});
+
+test('Проверка отображения несуществующего текста "тест"', async ({ page }) => {
+    await page.goto('http://localhost:3000/en');
+
+    const heading = page.locator('h1');
+    await expect(heading).toHaveText('test');
+});
