8b2665393e317ea51002cff9636223b76e817fab
kate krema <katekrema@MacBook-Pro-kate.local>
2024-11-01 17:40:07 +0300

    format:fix

--- frontend/components/Hint.spec.tsx
+++ frontend/components/Hint.spec.tsx
@@ -1,16 +1,16 @@
-import {render, screen} from "@testing-library/react";
-import { Hint } from "@/components/Hint";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from '@testing-library/react';
+import { Hint } from '@/components/Hint';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-describe("Hint component", () => {
-    it("renders with the correct label", async () => {
+describe('Hint component', () => {
+    it('renders with the correct label', async () => {
         render(
             <Hint label="Tooltip Label">
                 <button>Hover me</button>
-            </Hint>
+            </Hint>,
         );
-        await userEvent.hover(screen.getByText("Hover me"));
-        const tooltip = await screen.findByRole("tooltip");
-        expect(tooltip).toHaveTextContent("Tooltip Label");
+        await userEvent.hover(screen.getByText('Hover me'));
+        const tooltip = await screen.findByRole('tooltip');
+        expect(tooltip).toHaveTextContent('Tooltip Label');
     });
 });
--- frontend/components/Hint.tsx
+++ frontend/components/Hint.tsx
@@ -1,26 +1,26 @@
-"use client";
+'use client';
 import {
     Tooltip,
     TooltipContent,
     TooltipProvider,
     TooltipTrigger,
-} from "@/components/ui/Tooltip";
+} from '@/components/ui/Tooltip';
 export interface HintProps {
     label: string;
     children: React.ReactNode;
-    side?: "top" | "bottom" | "left" | "right";
-    align?: "start" | "center" | "end";
+    side?: 'top' | 'bottom' | 'left' | 'right';
+    align?: 'start' | 'center' | 'end';
     sideOffset?: number;
     alignOffset?: number;
 }
 export const Hint = ({
-                         label,
-                         children,
-                         side,
-                         align,
-                         sideOffset,
-                         alignOffset,
-                     }: HintProps) => {
+    label,
+    children,
+    side,
+    align,
+    sideOffset,
+    alignOffset,
+}: HintProps) => {
     return (
         <TooltipProvider>
             <Tooltip delayDuration={100}>
