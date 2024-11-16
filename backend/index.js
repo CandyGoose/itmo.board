1c93c2208de7e9a2e8cf3ff0d3cb419eba9ee059
BahrMikhev <39864162+BahrMikhev@users.noreply.github.com>
2024-11-16 21:39:41 +0300

    added basic MongoDB support

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
 const app = express();
+mongoose.connect('mongodb://localhost:27017/mango')
+  .then(()=> {
+    console.log('Database connected');
+  }).catch((error)=> {
+    console.log('Error connecting to database: '+ error);
+});
+app.use(bodyParser.json());
 
-const port = process.env.PORT;
+require('./views/Board.view')(app);
+
+const port = process.env.PORT || 4000;
 
 const server = app.listen(port, () => {
     console.log(`Server is running at site: http://localhost:${port}`);
