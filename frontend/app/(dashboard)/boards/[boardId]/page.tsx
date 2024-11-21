1054c0d3d0fabb7ce35c13b616442eaf4778db3d
BahrMikhev <39864162+BahrMikhev@users.noreply.github.com>
2024-11-21 08:08:44 +0300

    unsaved changes

--- frontend/app/(dashboard)/boards/[boardId]/page.tsx
+++ frontend/app/(dashboard)/boards/[boardId]/page.tsx
@@ -35,6 +35,10 @@ export default function BoardPage() {
     return (
         <div>
             <Canvas />
+            <div style="display: none;">
+                <CanvasLoading/>
+            </div>
         </div>
+        
     );
 }
