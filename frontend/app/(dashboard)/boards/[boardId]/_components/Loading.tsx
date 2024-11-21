6a1edb219371cc8f7b55b1b3fad0e7a6658b92b6
BahrMikhev <39864162+BahrMikhev@users.noreply.github.com>
2024-11-21 06:23:53 +0300

    linter fix

--- frontend/app/(dashboard)/boards/[boardId]/_components/Loading.tsx
+++ frontend/app/(dashboard)/boards/[boardId]/_components/Loading.tsx
@@ -1,13 +1,12 @@
-import { Loader } from "lucide-react"
+import { Loader } from 'lucide-react';
 
-import { ToolBarSkeleton } from "./Toolbar"
+import { ToolBarSkeleton } from './Toolbar';
 
 export const CanvasLoading = () => {
-
-    return(
+    return (
         <main className="w-full h-full relative bg-neutral-100 touch-none flex items-center justify-center">
-            <Loader className="h-6 w-6 text-muted-foreground animate-spin"/>
+            <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
             <ToolBarSkeleton />
         </main>
-    )
-}
+    );
+};
