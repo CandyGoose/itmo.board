878c18b70d3c301c4ed79a2e9adf5189fb34e942
kkrema <kremakate@gmail.com>
2024-11-28 10:59:43 +0300

    Refactor: Integrate API call for fetching boards and add board creation handler

---
+++ frontend/actions/Board.tsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export interface Board {
+    _id: string,
+    title: string,
+    orgId: string,
+    authorId: string,
+    imageUrl: string,
+    createdAt?: Date,
+}
+
+export async function getAllBoards(userId: string, orgId: string): Promise<Board[]> {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/boards/${userId}/${orgId}`);
+    return res.data ? res.data : undefined;
+}
