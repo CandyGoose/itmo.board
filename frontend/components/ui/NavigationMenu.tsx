1ca6652c46c9b22a3bdfc4b3f3f408dcee02d233
Goose <112972833+CandyGoose@users.noreply.github.com>
2024-12-11 22:12:12 +0300

    Добавление стилей для NavigationMenuTrigger

--- frontend/components/ui/NavigationMenu.tsx
+++ frontend/components/ui/NavigationMenu.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
 
 const NavigationMenu = React.forwardRef<
     React.ElementRef<typeof NavigationMenuPrimitive.Root>,
@@ -34,4 +35,10 @@ const NavigationMenuList = React.forwardRef<
 ));
 NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 
-export { NavigationMenu, NavigationMenuList };
+const NavigationMenuItem = NavigationMenuPrimitive.Item;
+
+const navigationMenuTriggerStyle = cva(
+    "group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-md font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+);
+
+export { NavigationMenu, NavigationMenuList, NavigationMenuItem, navigationMenuTriggerStyle };
