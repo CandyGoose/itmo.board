07797880243e21bbcd6d25feba4ba16e96c4b1c8
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-12-11 21:12:12 +0300

    Добавление компонента NavigationMenuList

--- frontend/components/ui/NavigationMenu.tsx
+++ frontend/components/ui/NavigationMenu.tsx
@@ -15,9 +15,23 @@ const NavigationMenu = React.forwardRef<
         {...props}
     >
         {children}
-        <NavigationMenuViewport />
     </NavigationMenuPrimitive.Root>
 ));
 NavigationMenu.displayName = NavigationMenuPrimitive.Root.displayName;
 
-export { NavigationMenu };
+const NavigationMenuList = React.forwardRef<
+    React.ElementRef<typeof NavigationMenuPrimitive.List>,
+    React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.List>
+>(({ className, ...props }, ref) => (
+    <NavigationMenuPrimitive.List
+        ref={ref}
+        className={cn(
+            "group flex flex-1 list-none items-center justify-center space-x-1",
+            className
+        )}
+        {...props}
+    />
+));
+NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
+
+export { NavigationMenu, NavigationMenuList };
