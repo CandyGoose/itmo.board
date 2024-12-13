eb7a9dae6692582526b6bda8ca0710fabee9ae01
Gleb Malikov Villegas ^-^ <44012947+glebmavi@users.noreply.github.com>
2024-12-13 20:15:47 +0300

    Line width input on same row

--- frontend/app/[locale]/(dashboard)/boards/[boardId]/_components/LineWidthInput.tsx
+++ frontend/app/[locale]/(dashboard)/boards/[boardId]/_components/LineWidthInput.tsx
@@ -1,4 +1,5 @@
 import React, { memo } from 'react';
+import { useTranslations } from 'next-intl';
 
 interface LineWidthInputProps {
     lineWidth: number;
@@ -7,27 +8,25 @@ interface LineWidthInputProps {
 
 export const LineWidthInput: React.FC<LineWidthInputProps> = memo(
     ({ lineWidth, onLineWidthChange }) => {
+        const t = useTranslations('tools');
         const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
             onLineWidthChange(e.target.value);
         };
 
         return (
-            <div className="flex flex-col mb-2">
-                <label className="text-sm mb-1">Line Width</label>{' '}
-                {/*TODO: Add translation*/}
+            <div className="flex items-center mb-2">
+                <label className="text-sm mr-2">{t('lineWidth')}</label>
                 <input
                     type="number"
                     value={lineWidth}
                     onChange={handleChange}
-                    className="border rounded p-1 text-sm"
+                    className="border rounded p-1 text-sm mr-2 w-14"
                     min={1}
                 />
-                <div className="mt-1 flex items-center">
-                    <div
-                        className="bg-black"
-                        style={{ width: '50px', height: `${lineWidth}px` }}
-                    />
-                </div>
+                <div
+                    className="bg-black"
+                    style={{ width: '40px', height: `${lineWidth}px` }}
+                />
             </div>
         );
     },
