645c6966c5e1e57698e24a2afe90e39fb5d6f349
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-12-13 15:23:12 +0300

    Добавление компонента AccordionContent

--- frontend/components/ui/Accordion.tsx
+++ frontend/components/ui/Accordion.tsx
@@ -3,6 +3,7 @@
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDownIcon } from "lucide-react"
+
 import { cn } from "@/lib/utils"
 
 const Accordion = AccordionPrimitive.Root
@@ -39,4 +40,18 @@ const AccordionTrigger = React.forwardRef<
 ))
 AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
 
-export { Accordion, AccordionItem, AccordionTrigger }
+const AccordionContent = React.forwardRef<
+    React.ElementRef<typeof AccordionPrimitive.Content>,
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+    <AccordionPrimitive.Content
+        ref={ref}
+        className="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+        {...props}
+    >
+        <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    </AccordionPrimitive.Content>
+))
+AccordionContent.displayName = AccordionPrimitive.Content.displayName
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
