5556db8b2ed69f21484f077c286dfe2b0a575bda
Goose <112972833+CandyGoose@users.noreply.github.com>
2024-12-15 01:39:01 +0300

    Дописаны методы бэка

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,44 @@
-const express = require('express');
 const mongoose = require("mongoose");
+const express = require("express");
 const bodyParser = require('body-parser');
 
-require('dotenv').config();
+require('dotenv').config()
 
 const app = express();
+
+const cors = require("cors");
+
+const port = process.env.PORT || 4000;
+
+const allowedOrigins = ['http://localhost:3000'];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    'http://localhost:3000',
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
 mongoose.connect('mongodb://localhost:27017/mango')
   .then(()=> {
     console.log('Database connected');
@@ -13,12 +47,11 @@ mongoose.connect('mongodb://localhost:27017/mango')
 });
 app.use(bodyParser.json());
 
-require('./views/Board.view')(app);
+require('./views/board.view')(app);
 
-const port = process.env.PORT || 4000;
-
-const server = app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running at site: http://localhost:${port}`);
 });
 
-module.exports = server;
+
+
--- backend/views/Board.view.js
+++ backend/views/Board.view.js
@@ -1,12 +1,19 @@
-const { getAll, createBoard, deleteBoard} = require("../controllers/Board")
+const { getAll, createBoard, getByOrgId, updateBoard, deleteBoard, getById, renameBoard } = require("../controllers/board")
 
 module.exports = function(app){
     //GET
     app.get('/boards', getAll);
+    app.get('/boards/:userId/:orgId', getByOrgId);
+    app.get('/boards/:boardId', getById);
 
     //POST
     app.post('/boards', createBoard);
 
+    //PUT   
+    app.put('/boards/:id', updateBoard)
+    app.put('/boards/title/:id', renameBoard)
+
     //DELETE
     app.delete('/boards/:id', deleteBoard);
 }
+
