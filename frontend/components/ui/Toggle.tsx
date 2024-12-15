63b196aeadf63a38a3c99734db549f6762b32704
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-12-15 17:13:33 +0300

    Добавление базовой структуры компонента Toggle

---
+++ frontend/components/ui/Toggle.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import * as React from "react"
+import * as TogglePrimitive from "@radix-ui/react-toggle"
+
+const Toggle = React.forwardRef<
+    React.ElementRef<typeof TogglePrimitive.Root>,
+    React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root>
+>(({ ...props }, ref) => (
+    <TogglePrimitive.Root ref={ref} {...props} />
+))
+
+Toggle.displayName = TogglePrimitive.Root.displayName
+
+export { Toggle }
