be4a8ed024aea409079581e4bd35a79bd919d554
kkrema <kremakate@gmail.com>
2024-12-16 05:58:48 +0300

    добавлены тесты для Info

--- frontend/app/[locale]/(dashboard)/boards/[boardId]/_components/Info.tsx
+++ frontend/app/[locale]/(dashboard)/boards/[boardId]/_components/Info.tsx
@@ -149,6 +149,9 @@ export const Info = ({
 
 export const InfoSkeleton = () => {
     return (
-        <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]" />
+        <div
+            role="status"
+            className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]"
+        />
     );
 };
