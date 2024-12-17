8c9ec31781bcf2b6280291b509ee67b2e95ad4be
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-12-17 20:49:21 +0300

    Добавлена функция расчёта размера шрифта

---
+++ frontend/app/[locale]/(dashboard)/boards/[boardId]/_components/Text.tsx
@@ -0,0 +1,19 @@
+import { Kalam } from "next/font/google";
+
+const font = Kalam({
+    subsets: ["latin"],
+    weight: ["400"],
+});
+
+const calculateFontSize = (width: number, height: number) => {
+    const maxFontSize = 72;
+    const scaleFactor = 0.25;
+    const fontSizeBasedOnHeight = height * scaleFactor;
+    const fontSizeBasedOnWidth = width * scaleFactor;
+
+    return Math.min(
+        fontSizeBasedOnHeight,
+        fontSizeBasedOnWidth,
+        maxFontSize
+    );
+}
