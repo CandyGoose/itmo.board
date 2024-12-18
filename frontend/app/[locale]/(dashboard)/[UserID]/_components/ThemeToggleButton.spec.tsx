2e22eca45a15690a576aa243c812020c3e41ea7b
kkrema <kremakate@gmail.com>
2024-12-18 23:27:19 +0300

    fix tests

---
+++ frontend/app/[locale]/(dashboard)/[UserID]/_components/ThemeToggleButton.spec.tsx
@@ -0,0 +1,38 @@
+import {render, screen} from "@testing-library/react";
+import { ThemeToggleButton } from "./ThemeToggleButton";
+import { useTheme } from "@/providers/ThemeProvider";
+import '@testing-library/jest-dom';
+
+jest.mock("@/providers/ThemeProvider", () => ({
+    useTheme: jest.fn(),
+}));
+
+describe("ThemeToggleButton", () => {
+    const setTheme = jest.fn();
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("отображает Sun иконку при светлой теме", () => {
+        (useTheme as jest.Mock).mockReturnValue({ theme: "light", setTheme });
+
+        render(<ThemeToggleButton />);
+
+        const button = screen.getByRole("button", { name: /toggle theme/i });
+
+        expect(button).toBeInTheDocument();
+        expect(screen.getByTestId("sun-icon")).toBeInTheDocument();
+    });
+
+    test("отображает Moon иконку при темной теме", () => {
+        (useTheme as jest.Mock).mockReturnValue({ theme: "dark", setTheme });
+
+        render(<ThemeToggleButton />);
+
+        const button = screen.getByRole("button", { name: /toggle theme/i });
+
+        expect(button).toBeInTheDocument();
+        expect(screen.getByTestId("moon-icon")).toBeInTheDocument();
+    });
+});
