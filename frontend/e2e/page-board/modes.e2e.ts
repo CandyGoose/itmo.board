b3782ccde87b9422197ba4070b796cc6e86410d4
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-05 16:56:34 +0300

    init coop

---
+++ frontend/e2e/page-board/modes.e2e.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test.describe('Modes', () => {
+    test('should restrict actions for read-only', async ({ page }) => {
+
+    });
+
+    test('should allow editing', async ({ page }) => {
+
+    });
+});
---
+++ frontend/e2e/page-board/multi-user.e2e.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test.describe('Multi-User Collaboration', () => {
+    test('should allow two users to draw on the same board', async ({ page }) => {
+
+    });
+
+    test('should update objects in real time for all users', async ({ page }) => {
+
+    });
+});
