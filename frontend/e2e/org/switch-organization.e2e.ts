47ac7093fa02261aed403c6d5c587b4979e4375d
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-05 16:47:23 +0300

    init switch

---
+++ frontend/e2e/org/switch-organization.e2e.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test.describe('Switch Organization', () => {
+    test('should switch to another organization', async ({ page }) => {
+
+    });
+
+    test('should return to the original organization', async ({ page }) => {
+
+    });
+});
