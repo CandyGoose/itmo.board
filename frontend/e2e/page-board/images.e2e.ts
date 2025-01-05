51635a6c82b70664119dad5e7c915f2db17015be
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-05 16:35:12 +0300

    init board tools

---
+++ frontend/e2e/page-board/images.e2e.ts
@@ -0,0 +1,15 @@
+import { test } from '@playwright/test';
+
+test.describe('Images', () => {
+    test('should upload an image to the board', async ({ page }) => {
+
+    });
+
+    test('should move the image on the board', async ({ page }) => {
+
+    });
+
+    test('should delete the image from the board', async ({ page }) => {
+
+    });
+});
