dddc2b297c98a67e062b904812d436d6d4249771
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-05 16:43:12 +0300

    init history

---
+++ frontend/e2e/page-board/history.e2e.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test.describe('History (Undo/Redo)', () => {
+    test('should undo the last action', async ({ page }) => {
+
+    });
+
+    test('should redo the last undone action', async ({ page }) => {
+
+    });
+});
---
+++ frontend/e2e/page-board/layers.e2e.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test.describe('Layers', () => {
+    test('should move an object to the front layer', async ({ page }) => {
+
+    });
+
+    test('should move an object to the back layer', async ({ page }) => {
+
+    });
+});
