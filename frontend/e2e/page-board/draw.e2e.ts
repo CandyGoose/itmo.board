14cd4de758c40bf7fd204073ab62819ed677707f
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-05 16:35:12 +0300

    init board tools

---
+++ frontend/e2e/page-board/draw.e2e.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test.describe('Draw', () => {
+    test('should draw a line on the board', async ({ page }) => {
+
+    });
+
+    test('should delete the drawn line', async ({ page }) => {
+
+    });
+});
---
+++ frontend/e2e/page-board/shapes.e2e.ts
@@ -0,0 +1,15 @@
+import { test } from '@playwright/test';
+
+test.describe('Shapes', () => {
+    test('should add a square to the board', async ({ page }) => {
+
+    });
+
+    test('should resize a square', async ({ page }) => {
+
+    });
+
+    test('should delete the square', async ({ page }) => {
+
+    });
+});
