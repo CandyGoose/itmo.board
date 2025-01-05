a18bd06da6212f7d97255dcd0d6b64f2b3aebb87
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-05 10:02:00 +0300

    login e2e

---
+++ frontend/e2e/login.e2e.ts
@@ -0,0 +1,29 @@
+import { test } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+test('should log in', async ({ page }) => {
+    const username = process.env.TEST_USERNAME;
+    const password = process.env.TEST_PASSWORD;
+
+    if (!username || !password) {
+        throw new Error(
+            'Environment variables TEST_USERNAME and TEST_PASSWORD must be set',
+        );
+    }
+
+    await page.goto('/');
+
+    await page.getByPlaceholder('Enter your email address').click();
+    await page.fill('input[name="identifier"]', username);
+
+    await page.getByRole('button', { name: 'Continue', exact: true }).click();
+    await page.getByPlaceholder('Enter your password').click();
+    await page.fill('input[name="password"]', password);
+
+    await page.getByRole('button', { name: 'Continue' }).click();
+    await page.getByRole('heading', { name: 'welcome to itmo.board' }).click();
+
+    await page.context().storageState({ path: 'storageState.json' });
+});
