f6c3a664dec95d642e9da89ddb05f097c28c9ab8
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-05 13:48:55 +0300

    init elements board

---
+++ frontend/e2e/page-board/elementsBoard.e2e.ts
@@ -0,0 +1,19 @@
+import { test } from '@playwright/test';
+
+test.describe('Board Elements', () => {
+    test('should display the pen tool', async ({ page }) => {
+
+    });
+
+    test('should display the shape tool', async ({ page }) => {
+
+    });
+
+    test('should display the undo and redo buttons', async ({ page }) => {
+
+    });
+
+    test('should display the export button', async ({ page }) => {
+
+    });
+});
