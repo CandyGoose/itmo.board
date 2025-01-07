991adac2d1fb9a0a3e9defbe961c292a9935a446
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-07 19:56:36 +0300

    global setup

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,11 +1,13 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+    globalSetup: require.resolve('./e2e/global-setup'),
     testMatch: '**/*.e2e.ts',
     testIgnore: '**/*.test.{ts,tsx}',
     workers: 1,
     use: {
         baseURL: 'http://localhost:3000',
+        storageState: 'storageState.json',
     },
     timeout: 30000,
 };
