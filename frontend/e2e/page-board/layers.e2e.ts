b682eec679d9ae22a9ce7d9175253b9ad2067428
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-10 17:14:34 +0300

    delete after test

--- frontend/e2e/page-board/layers.e2e.ts
+++ frontend/e2e/page-board/layers.e2e.ts
@@ -1,9 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, BrowserContext, Page } from '@playwright/test';
 
-test.use({ storageState: 'storageState.json' });
+let context: BrowserContext;
+let page: Page;
 
 test.describe('Layers', () => {
-    test.beforeEach(async ({ page }) => {
+    test.beforeAll(async ({ browser }) => {
+        context = await browser.newContext({ storageState: 'storageState.json' });
+        page = await context.newPage();
         await page.goto('/');
         await page.getByText('Untitle-0').click();
 
@@ -16,8 +19,21 @@ test.describe('Layers', () => {
         await page.waitForTimeout(500);
     });
 
-    test('should move an object to the front layer', async ({ page }) => {
+    test.afterAll(async () => {
         const rectangle  = page.locator('rect').nth(3);
+        const circle = page.locator('ellipse').nth(1);
+        const deleteButton = page
+            .locator('svg.lucide-trash2')
+            .locator('..');
+
+        await rectangle.dblclick();
+        await deleteButton.dblclick();
+        await circle.click();
+        await deleteButton.dblclick();
+    });
+
+    test('should move an object to the front layer', async () => {
+        const rectangle = page.locator('rect').nth(3);
         const circle = page.locator('ellipse').nth(1);
 
         await expect(rectangle).toBeVisible();
@@ -25,14 +41,10 @@ test.describe('Layers', () => {
 
         await rectangle.click();
 
-        const layersButton = page
-            .locator('svg.lucide-ellipsis-vertical')
-            .locator('..');
+        const layersButton = page.locator('svg.lucide-ellipsis-vertical').locator('..');
         await layersButton.click();
 
-        const bringToFront = page
-            .locator('svg.lucide-bring-to-front')
-            .locator('..');
+        const bringToFront = page.locator('svg.lucide-bring-to-front').locator('..');
         await bringToFront.click();
 
         const group = page.getByTestId('svg-group').nth(1);
