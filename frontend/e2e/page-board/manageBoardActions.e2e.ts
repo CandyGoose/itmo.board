be0dbdb9ce021ccf89837e57e24f4fcad057cd04
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-10 17:39:45 +0300

    copy

--- frontend/e2e/page-board/manageBoardActions.e2e.ts
+++ frontend/e2e/page-board/manageBoardActions.e2e.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.use({ storageState: 'storageState.json' });
 
@@ -7,9 +7,19 @@ test.describe('Board Actions', () => {
         test.beforeEach(async ({ page }) => {
             await page.goto('/');
             await page.getByText('Untitle-0').click();
+            const menuButton = page.locator('svg.lucide-menu').locator('..');
+            await expect(menuButton).toBeVisible();
+            await menuButton.click();
         });
 
-        test('should copy the board link to clipboard on board', async ({ page }) => {});
+        test('should copy the board link to clipboard on board', async ({ page }) => {
+            const copyLinkButton = page.getByRole('menuitem', { name: 'Copy link' });
+            await expect(copyLinkButton).toBeVisible();
+            await copyLinkButton.click();
+
+            const notification = page.getByRole('menuitem', { name: 'Copy link' });
+            await expect(notification).toBeVisible();
+        });
 
         test('should delete the board on board', async ({ page }) => {});
     });
