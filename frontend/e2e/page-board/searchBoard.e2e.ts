0d9b29c43589bf5f746458e6b463a9fb1b4798ab
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-10 14:12:33 +0300

    format fix

--- frontend/e2e/page-board/searchBoard.e2e.ts
+++ frontend/e2e/page-board/searchBoard.e2e.ts
@@ -3,15 +3,7 @@ import { test, expect } from '@playwright/test';
 test.use({ storageState: 'storageState.json' });
 
 test.describe('Create Board', () => {
-    test('should found board', async ({
-                                                                          page,
-                                                                      }) => {
+    test('should found board', async ({ page }) => {});
 
-    });
-
-    test('should show message if nothing found', async ({
-                                                                          page,
-                                                                      }) => {
-
-    });
+    test('should show message if nothing found', async ({ page }) => {});
 });
