fbd3d66e09228b352724fa821905370082a009fb
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-10 15:43:55 +0300

    add link images

--- frontend/e2e/page-board/images.e2e.ts
+++ frontend/e2e/page-board/images.e2e.ts
@@ -1,8 +1,40 @@
-import { expect, test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
+
+const imageURL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Lava_Lake_Nyiragongo_2.jpg/1200px-Lava_Lake_Nyiragongo_2.jpg?20110725120304';
+const downloadPath = path.resolve(__dirname, './temp/test.jpg');
 
 test.use({ storageState: 'storageState.json' });
 
 test.describe('Images', () => {
+    test.beforeAll(async () => {
+        if (!fs.existsSync(path.dirname(downloadPath))) {
+            fs.mkdirSync(path.dirname(downloadPath), { recursive: true });
+        }
+
+        const response = await axios({
+            url: imageURL,
+            method: 'GET',
+            responseType: 'stream',
+        });
+
+        const writer = fs.createWriteStream(downloadPath);
+        response.data.pipe(writer);
+
+        await new Promise((resolve, reject) => {
+            writer.on('finish', resolve);
+            writer.on('error', reject);
+        });
+    });
+
+    test.afterAll(() => {
+        if (fs.existsSync(downloadPath)) {
+            fs.unlinkSync(downloadPath);
+        }
+    });
+
     test.beforeEach(async ({ page }) => {
         await page.goto('/');
         await page.getByText('Untitle-0').click();
@@ -15,52 +47,39 @@ test.describe('Images', () => {
         await page.getByTestId('svg-element').nth(1).click();
         await page.locator('label').filter({ hasText: 'Select from computer' }).click();
 
-        const filePath = './e2e/img/test.jpg';
         const fileInput = page.locator('input[type="file"]');
-        await fileInput.setInputFiles(filePath);
+        await fileInput.setInputFiles(downloadPath);
 
         const image = page.locator('image').nth(1);
         await expect(image).toBeVisible();
+
+        await image.click();
+        const deleteButton = page.locator('svg.lucide-trash2').locator('..');
+        await deleteButton.click();
     });
 
-    test('should upload an image to the board from link', async ({ page }) => {});
+    test('should upload an image to the board from link', async ({ page }) => {
+        const uploadButton = page.locator('svg.lucide-image').locator('..');
+        await uploadButton.click();
 
-    test('should move the image on the board', async ({ page }) => {
+        await page.getByTestId('svg-element').nth(1).click();
+        await page.getByPlaceholder('https://example.com/image.png').fill(imageURL);
+        await page.getByRole('button', { name: 'Load' }).click();
+
         const image = page.locator('image').nth(1);
         await expect(image).toBeVisible();
-
-        const boundingBox = await image.boundingBox();
-        if (!boundingBox) {
-            throw new Error('Bounding box not found for image');
-        }
-
-        const startX = boundingBox.x + boundingBox.width / 2;
-        const startY = boundingBox.y + boundingBox.height / 2;
-        const endX = startX + 100;
-        const endY = startY + 100;
-
-        await page.mouse.click(startX, startY);
-
-        await page.mouse.move(startX, startY);
-        await page.mouse.down();
-        await page.mouse.move(endX, endY);
-        await page.mouse.up();
-
-        const newBoundingBox = await image.boundingBox();
-        expect(newBoundingBox?.x).toBeCloseTo(endX - boundingBox.width / 2, 1);
-        expect(newBoundingBox?.y).toBeCloseTo(endY - boundingBox.height / 2, 1);
     });
 
     test('should delete the image from the board', async ({ page }) => {
         const image = page.locator('image').nth(1);
-        expect(image).toBeVisible();
+        await expect(image).toBeVisible();
 
         await image.click();
 
         const deleteButton = page.locator('svg.lucide-trash2').locator('..');
-        expect(deleteButton).toBeVisible();
+        await expect(deleteButton).toBeVisible();
         await deleteButton.click();
 
-        expect(image).not.toBeVisible();
+        await expect(image).not.toBeVisible();
     });
 });
