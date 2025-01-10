d36bd1de6719fa3c18fe450a025aa783b5835ce2
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-10 13:27:45 +0300

    create search template

---
+++ frontend/e2e/page-board/searchBoard.e2e.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test.use({ storageState: 'storageState.json' });
+
+test.describe('Create Board', () => {
+    test('should found board', async ({
+                                                                          page,
+                                                                      }) => {
+
+    });
+
+    test('should show message if nothing found', async ({
+                                                                          page,
+                                                                      }) => {
+
+    });
+});
