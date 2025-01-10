a13434fdffa17068b81dca93b759d6312be58029
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-10 16:56:34 +0300

    front layer

--- frontend/e2e/page-board/layers.e2e.ts
+++ frontend/e2e/page-board/layers.e2e.ts
@@ -1,9 +1,46 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test.use({ storageState: 'storageState.json' });
 
 test.describe('Layers', () => {
-    test('should move an object to the front layer', async ({ page }) => {});
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+        await page.getByText('Untitle-0').click();
 
-    test('should move an object to the back layer', async ({ page }) => {});
+        await page.locator('svg.lucide-square').locator('..').click();
+        await page.mouse.click(100, 100);
+        await page.waitForTimeout(500);
+
+        await page.locator('svg.lucide-circle').locator('..').dblclick();
+        await page.mouse.click(100, 300);
+        await page.waitForTimeout(500);
+    });
+
+    test('should move an object to the front layer', async ({ page }) => {
+        const rectangle  = page.locator('rect').nth(3);
+        const circle = page.locator('ellipse').nth(1);
+
+        await expect(rectangle).toBeVisible();
+        await expect(circle).toBeVisible();
+
+        await rectangle.click();
+
+        const layersButton = page
+            .locator('svg.lucide-ellipsis-vertical')
+            .locator('..');
+        await layersButton.click();
+
+        const bringToFront = page
+            .locator('svg.lucide-bring-to-front')
+            .locator('..');
+        await bringToFront.click();
+
+        const group = page.getByTestId('svg-group').nth(1);
+
+        const lastElement = group.locator('rect, ellipse').last();
+        expect(await lastElement.evaluate(el => el.tagName.toLowerCase())).toBe('rect');
+
+        const firstElement = group.locator('rect, ellipse').first();
+        expect(await firstElement.evaluate(el => el.tagName.toLowerCase())).toBe('ellipse');
+    });
 });
