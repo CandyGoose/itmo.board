0b55f1ff56c5724035672c82aed65f01085f0e54
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-10 17:34:34 +0300

    struct init

--- frontend/e2e/page-board/manageBoardActions.e2e.ts
+++ frontend/e2e/page-board/manageBoardActions.e2e.ts
@@ -3,11 +3,32 @@ import { test } from '@playwright/test';
 test.use({ storageState: 'storageState.json' });
 
 test.describe('Board Actions', () => {
-    test('should copy the board link to clipboard', async ({ page }) => {});
+    test.describe('Board Actions on board', () => {
+        test.beforeEach(async ({ page }) => {
+            await page.goto('/');
+            await page.getByText('Untitle-0').click();
+        });
 
-    test('should rename the board', async ({ page }) => {});
+        test('should copy the board link to clipboard on board', async ({ page }) => {});
 
-    test('should navigate to the main menu', async ({ page }) => {});
+        test('should rename the board on board', async ({ page }) => {});
 
-    test('should delete the board', async ({ page }) => {});
+        test('should navigate to the main menu on board', async ({ page }) => {});
+
+        test('should delete the board on board', async ({ page }) => {});
+    });
+
+    test.describe('Board Actions in org', () => {
+        test.beforeEach(async ({ page }) => {
+            await page.goto('/');
+        });
+
+        test('should copy the board link to clipboard in org', async ({ page }) => {});
+
+        test('should rename the board in org', async ({ page }) => {});
+
+        test('should navigate to the main menu in org', async ({ page }) => {});
+
+        test('should delete the board in org', async ({ page }) => {});
+    });
 });
