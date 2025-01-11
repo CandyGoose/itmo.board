889203ddf14684c65ee8e07d8d6e441ed80d1497
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-11 15:17:34 +0300

    struct style

--- frontend/e2e/page-board/styles.e2e.ts
+++ frontend/e2e/page-board/styles.e2e.ts
@@ -1,14 +1,36 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.use({ storageState: 'storageState.json' });
 
-test.describe('Object Styles', () => {
+test.describe('Styles', () => {
     test.beforeEach(async ({ page }) => {
         await page.goto('/');
         await page.getByText('Untitle-0').click();
     });
+    test.describe('Styles List', () => {
+        test('should display styles menu', async ({ page }) => {});
 
-    test('should apply color to an object', async ({ page }) => {});
+        test('should display line width', async ({ page }) => {});
 
-    test('should change font size of text', async ({ page }) => {});
+        test('should display coordinates', async ({ page }) => {});
+
+        test('should display parameters', async ({ page }) => {});
+
+        test('should display font', async ({ page }) => {});
+
+        test('should display format', async ({ page }) => {});
+
+        test('should display alignment', async ({ page }) => {});
+    });
+
+
+    test.describe('Object Styles', () => {
+
+        test('should apply color to object', async ({ page }) => {});
+
+
+        test('should change font size of text', async ({ page }) => {});
+
+        test('should change font of text', async ({ page }) => {});
+    });
 });
