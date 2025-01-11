030167c1eb85d1716dda5c522990fc2078d32411
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-11 10:54:23 +0300

    edit modes

--- frontend/e2e/page-board/modes.e2e.ts
+++ frontend/e2e/page-board/modes.e2e.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.use({ storageState: 'storageState.json' });
 
@@ -7,8 +7,24 @@ test.describe('Modes', () => {
         await page.goto('/');
         await page.getByText('Untitle-0').click();
     });
-    
-    test('should restrict actions for read-only', async ({ page }) => {});
 
-    test('should allow editing', async ({ page }) => {});
+    test('should restrict actions for read-only', async ({ page }) => {
+        const toggleButton = page.locator('svg.lucide-pencil-ruler').locator('..');
+        await toggleButton.click();
+        await page.getByRole('menuitem', { name: 'View only' }).click();
+
+        const squareTool = page.locator('svg.lucide-square').locator('..');
+        const isSquareToolDisabled = await squareTool.isDisabled();
+        expect(isSquareToolDisabled).toBe(true);
+    });
+
+    test('should allow editing', async ({ page }) => {
+        const toggleButton = page.locator('svg.lucide-pencil-ruler').locator('..');
+        await toggleButton.click();
+        await page.getByRole('menuitem', { name: 'Edit mode' }).click();
+
+        const squareTool = page.locator('svg.lucide-square').locator('..');
+        const isSquareToolDisabled = await squareTool.isDisabled();
+        expect(isSquareToolDisabled).toBe(false);
+    });
 });
