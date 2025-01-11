5c9a8b5b4af4929ec3c3863508e826ead415ee48
CandyGoose <112972833+CandyGoose@users.noreply.github.com>
2025-01-11 17:23:10 +0300

    style menu tests

--- frontend/e2e/page-board/styles.e2e.ts
+++ frontend/e2e/page-board/styles.e2e.ts
@@ -1,7 +1,3 @@
-import { expect, test } from '@playwright/test';
-
-test.use({ storageState: 'storageState.json' });
-
 import { expect, test, BrowserContext, Page } from '@playwright/test';
 
 test.use({ storageState: 'storageState.json' });
@@ -23,19 +19,15 @@ test.describe('Styles', () => {
             .locator('..')
             .click();
 
-        const board = sharedPage.locator('svg[data-testid="svg-element"]');
         const clickX = 100;
         const clickY = 400;
 
         await sharedPage.mouse.click(clickX, clickY);
         await sharedPage.waitForTimeout(500);
-
-        const note = board.getByTestId('note-foreign-object').nth(1);
-        await expect(note).toBeVisible();
     });
 
     test.afterAll(async () => {
-        const note = sharedPage.getByText('New Note Content').nth(1);
+        const note = sharedPage.getByTestId('note-content-editable').nth(1)
         await note.dblclick();
 
         const deleteButton = sharedPage
@@ -49,20 +41,48 @@ test.describe('Styles', () => {
         await context.close();
     });
 
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+        await page.getByText('Untitle-0').click();
+
+        const note = page.getByTestId('note-foreign-object').nth(1);
+        await expect(note).toBeVisible();
+
+        await note.click();
+
+        await page.getByRole('button', { name: 'styles' }).click();
+    });
+
     test.describe('Styles List', () => {
-        test('should display styles menu', async ({ page }) => {});
+        test('should display line width', async ({ page }) => {
+            await expect(page.getByLabel('Line width')).toBeVisible();
+        });
 
-        test('should display line width', async ({ page }) => {});
+        test('should display coordinates', async ({ page }) => {
+            await expect(page.getByLabel('X', { exact: true })).toBeVisible();
+            await expect(page.getByLabel('Y', { exact: true })).toBeVisible();
+        });
 
-        test('should display coordinates', async ({ page }) => {});
+        test('should display parameters', async ({ page }) => {
+            await expect(page.getByTestId('selection-tools-container').locator('div').filter({ hasText: 'WidthHeight' }).locator('#input1')).toBeVisible();
+            await expect(page.getByTestId('selection-tools-container').locator('div').filter({ hasText: 'WidthHeight' }).locator('#input2')).toBeVisible();
+        });
 
-        test('should display parameters', async ({ page }) => {});
+        test('should display font', async ({ page }) => {
+            await expect(page.getByLabel('Font', { exact: true })).toBeVisible();
+        });
 
-        test('should display font', async ({ page }) => {});
+        test('should display format', async ({ page }) => {
+            await expect(page.getByLabel('Bold')).toBeVisible();
+            await expect(page.getByLabel('Italic')).toBeVisible();
+            await expect(page.getByLabel('Strike Through')).toBeVisible();
+        });
 
-        test('should display format', async ({ page }) => {});
-
-        test('should display alignment', async ({ page }) => {});
+        test('should display alignment', async ({ page }) => {
+            await expect(page.getByLabel('Left Aligned')).toBeVisible();
+            await expect(page.getByLabel('Center Aligned')).toBeVisible();
+            await expect(page.getByLabel('Right Aligned')).toBeVisible();
+        });
     });
 
     test.describe('Object Styles', () => {
