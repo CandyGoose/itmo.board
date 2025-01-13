749a42af8b458b02f8b34b8e7da9a0ecdd0aed92
kkrema <kremakate@gmail.com>
2025-01-14 01:07:46 +0300

    fix tests for Overlay

--- frontend/app/[locale]/(dashboard)/[UserID]/_components/board-card/Overlay.spec.tsx
+++ frontend/app/[locale]/(dashboard)/[UserID]/_components/board-card/Overlay.spec.tsx
@@ -1,20 +1,30 @@
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import { Overlay } from '@/app/[locale]/(dashboard)/[UserID]/_components/board-card/Overlay';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'; // Для matcher'ов, таких как toHaveClass
+import userEvent from '@testing-library/user-event';
+import { Overlay } from './Overlay';
 
-describe('Overlay Component', () => {
-    test('renders with default classes', () => {
+describe('Overlay component', () => {
+    it('renders the overlay with correct initial classes', () => {
         render(<Overlay />);
-
-        const overlayElement = document.getElementById('overlay');
-
+        const overlayElement = screen.getByTestId('overlay');
         expect(overlayElement).toBeInTheDocument();
         expect(overlayElement).toHaveClass(
-            'opacity-0',
-            'group-hover:opacity-50',
-            'transition-opacity',
-            'h-full',
-            'w-full',
+            'opacity-0 group-hover:opacity-30 transition-opacity h-full w-full bg-black'
         );
+    });
+
+    it('responds to hover', async () => {
+        render(
+            <div className="group">
+                <Overlay />
+            </div>
+        );
+        const overlayElement = screen.getByTestId('overlay');
+
+        // Simулируем наведение мыши на элемент
+        await userEvent.hover(overlayElement);
+
+        // Проверяем наличие класса
+        expect(overlayElement).toHaveClass('group-hover:opacity-30');
     });
 });
