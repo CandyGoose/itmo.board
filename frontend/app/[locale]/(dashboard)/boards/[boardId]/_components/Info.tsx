dc501e03c6b216595a1cff875b04e94a15a7603b
kkrema <kremakate@gmail.com>
2025-01-13 03:14:26 +0300

    Подключение новой логики Convex для получения данных о доске

--- frontend/app/[locale]/(dashboard)/boards/[boardId]/_components/Info.tsx
+++ frontend/app/[locale]/(dashboard)/boards/[boardId]/_components/Info.tsx
@@ -1,154 +1,93 @@
 'use client';
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '@/components/ui/Button';
-import { cn } from '@/lib/utils';
-import { Poppins } from 'next/font/google';
-import { Hint } from '@/components/Hint';
-import { useRenameModal } from '@/store/useRenameModal';
-import { Actions } from '@/components/Action';
-import { Eye, Menu, PencilRuler } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { Board, getBoardById } from '@/actions/Board';
-import { useEffect, useState } from 'react';
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from '@/components/ui/DropdownMenu';
-import { useOrganization } from '@clerk/nextjs';
-import CanvasSaver from '@/actions/CanvasSaver';
-import { useTranslations } from 'next-intl';
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { Skeleton } from "@/components/ui/Skeleton";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { Id } from "@/convex/_generated/dataModel";
+import { Button } from "@/components/ui/Button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import Hint from '@/components/Hint';
+import { useRenameModal } from "@/store/useRenameModal";
+import { Actions } from "@/components/Action";
+import { Menu } from "lucide-react";
 
 const font = Poppins({
-    subsets: ['latin'],
-    weight: ['600'],
+    subsets: ["latin"],
+    weight: ["600"],
 });
 
+interface InfoProps {
+    boardId: string;
+}
+
 const TabSeparator = () => {
-    return <div className="text-neutral-300 px-1.5">|</div>;
+    return <div className="text-gray-300 px-1 text-2xl">|</div>;
 };
 
-export const Info = ({
-    boardId,
-    editable,
-    setEditable,
-}: {
-    boardId: string;
-    editable: boolean;
-    setEditable: (value: boolean) => void;
-}) => {
+export const Info = ({ boardId }: InfoProps) => {
     const { onOpen } = useRenameModal();
-    const [board, setBoard] = useState<Board>();
-    const router = useRouter();
-    const t = useTranslations('tools');
+    const data = useQuery(api.Board.get, { id: boardId as Id<"boards"> });
 
-    const { membership } = useOrganization();
-
-    useEffect(() => {
-        const request = async () => {
-            const response = await getBoardById(boardId);
-            if (response) {
-                setBoard(response);
-            } else {
-                router.back();
-            }
-        };
-        request();
-    }, [boardId, router]);
-
-    if (!board) return <InfoSkeleton />;
+    if (!data) {
+        return <InfoSkeleton />;
+    }
 
     return (
-        <div className="flex flex-row gap-x-2 absolute top-2 left-2 items-center">
-            <div className="rounded-md px-1.5 h-12 flex items-center shadow-md bg-[var(--background-color)]">
-                <Hint label="Go to boards" side="bottom" sideOffset={10}>
-                    <Button asChild className="px-2" variant="board">
-                        <Link href="/">
-                            <Image
-                                alt="logo"
-                                src={'/logo.svg'}
-                                width={30}
-                                height={30}
-                            />
-                            <span
-                                className={cn(
-                                    'font-semibold text-xl ml-2 text-[var(--text-color)]',
-                                    font.className,
-                                )}
-                            >
-                                {t('board')}
-                            </span>
-                        </Link>
-                    </Button>
-                </Hint>
-                <TabSeparator />
-                <Hint label="Rename" side="bottom" sideOffset={10}>
-                    <Button
-                        variant="board"
-                        className="text-base font-normal px-2"
-                        onClick={() => onOpen(board._id, board.title)}
-                        disabled={!membership}
-                    >
-                        {board.title}
-                    </Button>
-                </Hint>
-                <TabSeparator />
-                <Actions
-                    disable={!membership}
-                    id={board._id}
-                    title={board.title}
-                    side="bottom"
-                    sideOffset={10}
+        <div className="absolute top-2 left-2 flex items-center py-2 px-2 bg-[var(--background-color)] rounded-lg shadow-md">
+            <Hint
+                label="Go to boards"
+                side="bottom"
+                sideOffset={15}
+                tooltipArrow={false}
+            >
+                <Button asChild className="px-2 " variant="board">
+                    <Link href="/">
+                        <Image src="/logo.svg" alt="board logo" width={40} height={40} />
+                        <span
+                            className={cn(
+                                "font-semibold text-xl ml-2",
+                                font.className
+                            )}
+                        >
+              Board
+            </span>
+                    </Link>
+                </Button>
+            </Hint>
+            <TabSeparator />
+            <Hint
+                label="Click to rename"
+                side="bottom"
+                sideOffset={15}
+                tooltipArrow={false}
+                deleyDuration={300}
+            >
+                <Button
+                    variant="board"
+                    className="text-base font-medium px-2"
+                    onClick={() => onOpen(data._id, data.title)}
                 >
-                    <div>
-                        <Hint label="Main menu" side="bottom" sideOffset={10}>
-                            <Button size="icon" variant="board">
-                                <Menu />
-                            </Button>
-                        </Hint>
-                    </div>
-                </Actions>
-                <div style={{ display: 'none' }}>
-                    <CanvasSaver boardId={boardId} />
+                    {data.title}
+                </Button>
+            </Hint>
+            <TabSeparator />
+            <Actions id={data._id} title={data.title} side="bottom" sideOffset={15}>
+                <div>
+                    <Hint
+                        label="Main menu"
+                        side="bottom"
+                        sideOffset={15}
+                        tooltipArrow={false}
+                    >
+                        <Button size="icon" variant="board">
+                            <Menu />
+                        </Button>
+                    </Hint>
                 </div>
-            </div>
-
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button
-                        variant="outline"
-                        className="shadow-md h-12 w-12 p-0 rounded-md"
-                    >
-                        <Eye
-                            className={cn(
-                                'h-[1.4rem] w-[1.4rem] rotate-0 opacity-0 scale-0 transition-all',
-                                !editable &&
-                                    '-rotate-180 scale-100 opacity-100',
-                            )}
-                        />
-                        <PencilRuler
-                            className={cn(
-                                'absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 opacity-0 transition-all',
-                                editable && '-rotate-0 scale-100 opacity-100',
-                            )}
-                        />
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="start">
-                    <DropdownMenuItem onClick={() => setEditable(false)}>
-                        {t('viewOnly')}
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                        onClick={() => setEditable(true)}
-                        disabled={!membership}
-                    >
-                        {t('editMode')}
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+            </Actions>
         </div>
     );
 };
