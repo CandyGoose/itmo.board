045c47e5d512a6816c092eac111afe42402cf3ba
kkrema <kremakate@gmail.com>
2025-01-13 03:21:56 +0300

    Добавляются новые данные (через Convex)

--- frontend/app/[locale]/(dashboard)/[UserID]/_components/board-card/Index.tsx
+++ frontend/app/[locale]/(dashboard)/[UserID]/_components/board-card/Index.tsx
@@ -1,60 +1,42 @@
 'use client';
 
-import { formatDistanceToNow } from 'date-fns';
-import { Skeleton } from '@/components/ui/Skeleton';
-import { Overlay } from './Overlay';
-import { Footer } from './Footer';
-import { useEffect, useState } from 'react';
-import { useParams, useRouter } from 'next/navigation';
-import { useTranslations } from 'next-intl';
-import { useClerk } from '@clerk/nextjs';
-import Image from 'next/image';
-import { Actions } from '@/components/Action';
-import { MoreHorizontal } from 'lucide-react';
-import CanvasSaver from '@/actions/CanvasSaver';
+import Image from "next/image";
+import Link from "next/link";
+import { Overlay } from "./Overlay";
+
+import { formatDistanceToNow } from "date-fns";
+import { useAuth } from "@clerk/nextjs";
+import { Footer } from "./Footer";
+import { Skeleton } from "@/components/ui/Skeleton";
+import { Actions } from "@/components/Action";
+import { MoreHorizontal } from "lucide-react";
+import {useState} from "react";
+import {router} from "next/client";
 
 interface BoardCardProps {
     id: string;
     title: string;
-    authorId: string;
-    createdAt: Date;
     imageUrl: string;
+    authorId: string;
+    authorName: string;
+    createdAt: number;
     orgId: string;
 }
 
-export const BoardCard = ({
-    id,
-    title,
-    authorId,
-    createdAt,
-    imageUrl,
-}: BoardCardProps) => {
-    const t = useTranslations('utils');
-    const router = useRouter();
-    const params = useParams();
-    const { user } = useClerk();
-    const [authorLabel, setAuthorLabel] = useState(
-        params.UserID === authorId ? t('you') : t('teammate'),
-    );
+const BoardCard = ({
+                       id,
+                       title,
+                       imageUrl,
+                       authorId,
+                       authorName,
+                       createdAt,
+                       orgId,
+                   }: BoardCardProps) => {
+    const { userId } = useAuth();
+    const authorLabel = userId === authorId ? "You" : authorName;
+    const createdLabel = formatDistanceToNow(createdAt, { addSuffix: true });
+
     const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        const getFirstName = async (userID: string) => {
-            if (userID === authorId) {
-                setAuthorLabel(t('you'));
-            } else if (user) {
-                setAuthorLabel(user.firstName || t('teammate'));
-            } else {
-                setAuthorLabel(t('teammate'));
-            }
-        };
-
-        getFirstName(params.UserID as string);
-    }, [params.UserID, authorId, t, user]);
-
-    const createdAtLabel = formatDistanceToNow(new Date(createdAt), {
-        addSuffix: true,
-    });
 
     const onClick = () => {
         try {
@@ -68,31 +50,32 @@ export const BoardCard = ({
     };
 
     return (
-        <div
-            className="group aspect-[100/127] border rounded-lg flex cursor-pointer
-            flex-col justify-between overflow-hidden relative"
-            data-testid={`board-card-${id}`} // Уникальный data-testid
-            onClick={onClick}
-        >
-            <div className="relative flex-1">
-                <Image src={imageUrl} alt="" fill className="object-fit" />
-                <Overlay />
-                <Actions id={id} title={title} side="right">
-                    <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
-                        <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
-                    </button>
-                </Actions>
-                <div style={{ display: 'none' }}>
-                    <CanvasSaver boardId={id} />
+        <Link href={`/boards/${id}`}>
+            <div className="group aspect-[98/120] border rounded-lg flex cursor-pointer
+            flex-col justify-between overflow-hidden relative" onClick={onClick}>
+                <div className="relative bg-amber-50 flex-1">
+                    <Image
+                        src={imageUrl}
+                        fill
+                        alt={title}
+                        className="object-fit"
+                        sizes="100vw"
+                    />
+                    <Overlay />
+                    <Actions id={id} title={title} side="right">
+                        <button className="absolute z-50 top-1 right-2 ">
+                            <MoreHorizontal className="text-black group-hover:text-white" />
+                        </button>
+                    </Actions>
                 </div>
+                <Footer
+                    title={title}
+                    authorLabel={authorLabel}
+                    createdLabel={createdLabel}
+                    disabled={loading}
+                />
             </div>
-            <Footer
-                title={title}
-                authorLabel={authorLabel}
-                createdAtLabel={createdAtLabel}
-                disabled={loading}
-            />
-        </div>
+        </Link>
     );
 };
 
@@ -100,9 +83,11 @@ BoardCard.Skeleton = function BoardCardSkeleton() {
     return (
         <div
             data-testid="board-card-skeleton"
-            className="aspect-[100/127] rounded-lg overflow-hidden"
+            className="aspect-[98/120] rounded-lg overflow-hidden"
         >
             <Skeleton className="h-full w-full" />
         </div>
     );
 };
+
+export default BoardCard;
